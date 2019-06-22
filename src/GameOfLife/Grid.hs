2640c4f3a647982727514ef87c349b46c8cb8e48
Tom Hulihan <hulihan.tom159@gmail.com>
2019-06-22 15:33:01 -0400

    Add Grid.foldWithCoord

--- src/GameOfLife/Grid.hs
+++ src/GameOfLife/Grid.hs
@@ -3,6 +3,7 @@ module GameOfLife.Grid
   , blank
   , random
   , next
+  , foldWithCoord
   ) where
 
 import Prelude hiding (replicate)
@@ -12,16 +13,16 @@ import Data.Tuple (swap)
 import System.Random (RandomGen)
 import qualified System.Random as R
 
-import Data.Vector.Unboxed (Vector, (!), imap, replicate, unfoldrN)
+import Data.Vector.Unboxed (Vector, (!), imap, ifoldl', replicate, unfoldrN)
 
 import GameOfLife.Cell (Cell, dead, isAlive, toCell)
 
 -- Represent the grid using a single (unboxed) 'Vector' of 'Cell's. I went with
 -- a single vector instead of a matrix to avoid boxing.
 data Grid =
-  Grid { cells :: Vector Cell
-       , width :: Int
-       , height :: Int
+  Grid { cells :: !(Vector Cell)
+       , width :: !Int
+       , height :: !Int
        } deriving (Eq, Show)
 
 -- Build a new blank 'Grid'.
@@ -39,10 +40,10 @@ random gen x y =
 
 -- Generate the next iteration of the 'Grid'.
 next :: Grid -> Grid
-next (Grid cs w h) =
+next grid@(Grid cs w h) =
   let
     nextCell idx cell
-      | isAlive cell = toCell $ covers 2 3 $ liveNeighbors idx
+      | isAlive cell = toCell . covers 2 3 $ liveNeighbors idx
       | otherwise = toCell $ liveNeighbors idx == 3
 
     covers lower upper subject =
@@ -51,33 +52,39 @@ next (Grid cs w h) =
     liveNeighbors :: Int -> Int
     liveNeighbors idx =
       let
-        (x, y) = toXY idx
+        (x, y) = toXY grid idx
         left
           | x == 0 = 0
-          | cellAlive $ toIdx (x - 1) y = 1
+          | cellAlive $ toIdx grid (x - 1) y = 1
           | otherwise = 0
         right
           | (x + 1) == w = 0
-          | cellAlive $ toIdx (x + 1) y = 1
+          | cellAlive $ toIdx grid (x + 1) y = 1
           | otherwise = 0
         up
           | y == 0 = 0
-          | cellAlive $ toIdx x (y - 1) = 1
+          | cellAlive $ toIdx grid x (y - 1) = 1
           | otherwise = 0
         down
           | (y + 1) == h = 0
-          | cellAlive $ toIdx x (y + 1) = 1
+          | cellAlive $ toIdx grid x (y + 1) = 1
           | otherwise = 0
       in
         left + right + up + down
 
     cellAlive idx =
       isAlive $ cs ! idx
-
-    toXY idx =
-      swap $ idx `divMod` h
-
-    toIdx x y =
-      x + (y * h)
   in
     Grid (imap nextCell cs) w h
+
+foldWithCoord :: (a -> Int -> Int -> Cell -> a) -> a -> Grid -> a
+foldWithCoord f initial grid@(Grid cs _ _) =
+  ifoldl' (\acc -> uncurry (f acc) . toXY grid) initial cs
+
+toXY :: Grid -> Int -> (Int, Int)
+toXY (Grid _ _ h) idx =
+  swap $ idx `divMod` h
+
+toIdx :: Grid -> Int -> Int -> Int
+toIdx (Grid _ _ h) x y =
+  x + (y * h)
