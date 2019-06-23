5d7ba3d7f3c479f8ad7ec1f1109448499c1229af
Tom Hulihan <hulihan.tom159@gmail.com>
2019-06-23 14:02:50 -0400

    Use separate queues for the event handler and timer to ui queues
    If the timer gets backed up (because it's enqueueing too many events),
    the UI can can still force a refresh / shutdown. This allows use to
    gracefully shutdown all of the threads.

--- src/GameOfLife/App.hs
+++ src/GameOfLife/App.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
+
 module GameOfLife.App
   ( runApp
   ) where
 
-import Control.Concurrent.STM (atomically)
-import Control.Concurrent.STM.TQueue (TQueue, flushTQueue, newTQueueIO, readTQueue, writeTQueue)
-import Control.Concurrent.Async (async, cancel, wait)
+import Control.Concurrent.STM (STM, atomically, retry)
+import Control.Concurrent.STM.TQueue (TQueue, flushTQueue, newTQueueIO, tryReadTQueue, writeTQueue)
+import Control.Concurrent.Async (async, wait)
 
 import qualified Graphics.Vty as V
 import Graphics.Vty.Config (defaultConfig)
@@ -20,26 +22,26 @@ runApp :: IO ()
 runApp = do
   gen <- newStdGen
 
-  timerQueue <- newTQueueIO
-  uiQueue <- newTQueueIO
+  eventToTimer <- newTQueueIO
+  eventToUI <- newTQueueIO
+  timerToUI <- newTQueueIO
 
   vty <- V.mkVty defaultConfig
   size <- displayBounds $ V.outputIface vty
 
-  eventHandlerFuture <- async $ eventHandlerThread vty timerQueue uiQueue
-  timerFuture <- async $ timerThread timerQueue uiQueue
-  uiFuture <- async $ uiThread vty uiQueue gen size
+  eventHandlerFuture <- async $ eventHandlerThread vty eventToTimer eventToUI
+  timerFuture <- async $ timerThread eventToTimer timerToUI
+  uiFuture <- async $ uiThread vty [eventToUI, timerToUI] gen size
 
   wait eventHandlerFuture
-
-  cancel timerFuture
-  cancel uiFuture
+  wait timerFuture
+  wait uiFuture
 
   V.shutdown vty
 
 eventHandlerThread :: V.Vty -> TQueue TimerControl -> TQueue UIControl -> IO ()
 eventHandlerThread vty timerQueue uiQueue =
-  runEventHandler $
+  runEventHandler
     EventHandlerConf { nextEvent = V.nextEvent vty
                      , timerCtrl = atomically . writeTQueue timerQueue
                      , uiCtrl = atomically . writeTQueue uiQueue
@@ -47,17 +49,28 @@ eventHandlerThread vty timerQueue uiQueue =
 
 timerThread :: TQueue TimerControl -> TQueue UIControl -> IO ()
 timerThread timerQueue uiQueue =
-  runTimer $
+  runTimer
     TimerConf { action = atomically $ writeTQueue uiQueue NextIteration
               , usDelay = 500 * 1000 -- 1/2 second
               , readCtrlMsgs = atomically $ flushTQueue timerQueue
               }
 
-uiThread :: RandomGen g => V.Vty -> TQueue UIControl -> g -> (Int, Int) -> IO ()
-uiThread vty uiQueue gen size =
-  runUI $
-    UIConf { nextControl = atomically $ readTQueue uiQueue
+uiThread :: RandomGen g => V.Vty -> [TQueue UIControl] -> g -> (Int, Int) -> IO ()
+uiThread vty queues gen size =
+  runUI
+    UIConf { nextControl = atomically $ readQueues queues
            , render = V.update vty
            , randGen = gen
            , initialSize = size
            }
+
+-- Tried to read from the queues in order, retrying until one is successful.
+readQueues :: [TQueue a] -> STM a
+readQueues =
+  foldr readOrElse retry
+
+readOrElse :: TQueue a -> STM a -> STM a
+readOrElse queue alternative =
+  tryReadTQueue queue >>= \case
+    Nothing -> alternative
+    Just val -> pure val
